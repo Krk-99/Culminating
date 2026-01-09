d50e8ff7a839e1f7c7f97389b9b100753f2f0629
Krk-99 <krisrk2009@gmail.com>
2026-01-08 21:47:36 -0500

    Movement changed to velocity yay

--- main.js
+++ main.js
@@ -17,8 +17,17 @@ let input = {
     jump: false
 }
 
+let nomove;
+
 // Gravity and physics variables
-let gravity = -9.81;
+let gravity = -10;
+let height = new BABYLON.Vector3(0,-1,0)
+let timer = false;
+let timer2 = false;
+let timercount = 0;
+let timercount2 = 0;
+let speed = 0;
+let drop = 0;
 let velocity = new BABYLON.Vector3(0, 0, 0);
 let isGrounded = false;
 
@@ -52,26 +61,29 @@ const createScene = function() {
 const scene = createScene();
 
 engine.runRenderLoop(function() {
+    if (timer) timercount++;
     mouselocked = engine.isPointerLock;
-    box.moveWithCollisions(new BABYLON.Vector3(0, -0.3, 0), false)
+    // box.moveWithCollisions(new BABYLON.Vector3(0, -0.3, 0), false)
     // console.log(box.position.y)
-    let directionf = box.forward;
-    let directionr = box.right;
-    let directionl = directionr.scale(-1);
-    let directionb = directionf.scale(-1);
-    if (input.forward) {
-        box.position.addInPlace(directionf.scale(0.1));
-    } else if (input.backward) {
-        box.position.addInPlace(directionb.scale(0.1));
-    }
-    if (input.left) {
-        box.position.addInPlace(directionl.scale(0.1));
-    } else if (input.right) {
-        box.position.addInPlace(directionr.scale(0.1));
-    }
-    if (input.jump) {
-        box.position.y += 1
-    }
+    // let directionf = box.forward;
+    // let directionr = box.right;
+    // let directionl = directionr.scale(-1);
+    // let directionb = directionf.scale(-1);
+    // if (input.forward) {
+    //     box.position.addInPlace(directionf.scale(0.1));
+    // } else if (input.backward) {
+    //     box.position.addInPlace(directionb.scale(0.1));
+    // }
+    // if (input.left) {
+    //     box.position.addInPlace(directionl.scale(0.1));
+    // } else if (input.right) {
+    //     box.position.addInPlace(directionr.scale(0.1));
+    // }
+    // if (input.jump) {
+    //     box.position.addInPlace(new BABYLON.Vector3(0, 1, 0).scale(1))
+    // }
+    box.moveWithCollisions(height.scale(drop))
+    box.moveWithCollisions(velocity.scale(speed))
     scene.render();
 });
 
@@ -83,6 +95,7 @@ window.addEventListener('resize', function() {
 
 const box = scene.getMeshByName('player');
 const camera = scene.getCameraByName('PlayerCamera');
+const ground = scene.getMeshByName('ground')
 camera.parent = box;
 
 
@@ -93,6 +106,66 @@ canvas.onclick = function() {
     }
 };
 
+scene.onBeforeRenderObservable.add(() => {
+    nomove = !input.forward && !input.backward && !input.left && !input.right;
+    if (nomove) {
+        timer = false
+        timercount = 0;
+    }
+    velocity.set(0,0,0)
+    if (input.jump) box.position.y = 1;
+    if (input.forward) {
+        const directionf = box.getDirection(new BABYLON.Vector3(0, 0, 1))
+        velocity.addInPlace(directionf)
+    } else if (input.backward) {
+        const directionb = box.getDirection(new BABYLON.Vector3(0,0, -1))
+        velocity.addInPlace(directionb)
+    }
+    if (input.left) {
+        const directionl = box.getDirection(new BABYLON.Vector3(-1, 0, 0))
+        velocity.addInPlace(directionl)
+    } else if (input.right) {
+        const directionr = box.getDirection(new BABYLON.Vector3(1, 0, 0))
+        velocity.addInPlace(directionr)
+    }
+    speed = timercount/400
+    // console.log(timercount)
+    // console.log(velocity)
+    speed = BABYLON.Scalar.Clamp(speed, 0, 0.5)
+    console.log(speed)
+});
+
+scene.onBeforeRenderObservable.add(() => {
+    const origin = box.position.clone()
+    origin.y -= box.ellipsoid.y
+    const direction = new BABYLON.Vector3(0, -1, 0)
+    const ray = new BABYLON.Ray(origin, direction, 1)
+    const hit = scene.pickWithRay(ray, (box) => {
+        return box === ground;
+    });
+    if (hit.hit) {
+        isGrounded = true
+    }else {
+        isGrounded = false
+    }
+    console.log(isGrounded)
+    if (!isGrounded) {
+        startTimer2();  
+        drop = gravity * timercount2/100
+    }  
+});
+
+const startTimer = function() {
+    timer = true;
+    timercount = 0;
+}
+const startTimer2 = function() {
+    if (timer2) {
+        return
+    };
+    timer2 = true;
+    timercount2 = 0;
+}
 
 scene.onPointerObservable.add((pointerInfo) => {
     if (mouselocked) {
@@ -102,23 +175,12 @@ scene.onPointerObservable.add((pointerInfo) => {
                 movementX = event.movementX || 0;
                 movementY = event.movementY || 0;
                 // console.log(`MovementX: ${movementX}, MovementY: ${movementY}`);
-                // camera.position.y += movementY/20
-                // camera.rotation.x -= -(BABYLON.NormalizeRadians(movementY * 0.009));
+                camera.position.y += movementY/200
+                camera.position.y = BABYLON.Scalar.Clamp(camera.position.y, 0.4, 3.2)
+                camera.rotation.x -= -(BABYLON.NormalizeRadians(movementY * 0.0009));
+                camera.rotation.x = BABYLON.Scalar.Clamp(camera.rotation.x, 0.208, 0.769)
                 // console.log(camera.rotation.x)
                 // console.log(camera.position.y)
-                // if (camera.position.y <= 3.2 && camera.rotation.x <=0.769 ){
-                //     if (camera.position.y >= 0.4 && camera.rotation.x >= 0.208) {
-                //     } else {
-                //         // camera.position.y = 0.4
-                //         // camera.rotation.x = 0.208
-                //     }
-                // } else {
-                //     // camera.position.y = 3.2
-                //     // camera.rotation.x = 0.769
-                //     // if (camera.position.y < 1) {
-                //     // } else {
-                //     // }
-                // }
                 box.rotation.y += movementX / 500;
                 break;
         }
@@ -129,18 +191,22 @@ scene.onPointerObservable.add((pointerInfo) => {
 window.addEventListener('keydown', function(event) {;
     if (event.key === 'w') {
         input.forward = true;
+        if (!timer) startTimer();
         // inputs = true;
     }
     if (event.key === 's') {
         input.backward = true;
+        if (!timer) startTimer();
         // inputs = true;
     }
     if (event.key === 'a') {
         input.left = true;
+        if (!timer) startTimer();
         // inputs = true;
     }
     if (event.key === 'd') {
         input.right = true;
+        if (!timer) startTimer();
         // inputs = true;  
     }
     if (event.key === ' ') {
