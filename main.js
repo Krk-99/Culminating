13dbfeddeb05626f1a4c2c7a943f88b166573226
Krk-99 <krisrk2009@gmail.com>
2026-01-11 17:22:35 -0500

    Made gravitational acceleration and a jumping velocity

--- main.js
+++ main.js
@@ -14,14 +14,16 @@ let input = {
     backward: false,
     left: false,
     right: false,
-    jump: false
+    jump: false,
+    shift: false
 }
+
 
 let nomove;
 
 // Gravity and physics variables
-let gravity = -10;
-let height = new BABYLON.Vector3(0,-1,0)
+let gravity = 10;
+let height = new BABYLON.Vector3(0,0,0)
 let timer = false;
 let timer2 = false;
 let timercount = 0;
@@ -30,6 +32,9 @@ let speed = 0;
 let drop = 0;
 let velocity = new BABYLON.Vector3(0, 0, 0);
 let isGrounded = false;
+let jumpcount = 5
+let jump = false;
+ 
 
 const createScene = function() {
     // Camera and light
@@ -39,21 +44,27 @@ const createScene = function() {
     const camera = new BABYLON.UniversalCamera('PlayerCamera', new BABYLON.Vector3(0, 2, -3), scene);
     camera.rotation.x = Math.PI / 5.5;
     // Temporary box for player
-    const box = BABYLON.MeshBuilder.CreateBox('player', {}, scene);
+    const box = BABYLON.MeshBuilder.CreateSphere('player', {}, scene);
     box.position.z = 3;
     box.ellipsoid = new BABYLON.Vector3(0.5, 0.5, 0.5)
+    height.addInPlace(box.getDirection(new BABYLON.Vector3(0, -1, 0)))
     box.checkCollisions = true; 
     camera.checkCollisions = true; 
     camera.ellipsoid = new BABYLON.Vector3(0.5, 0.5, 0.5)
-    const axis = new BABYLON.AxesViewer(scene, 2);
-    axis.xAxis.parent = box;
-    axis.yAxis.parent = box;
-    axis.zAxis.parent = box;
     const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {}, scene);
     sphere.position.z = -3;
     const ground = BABYLON.MeshBuilder.CreateGround('ground', {width: 10, height: 10}, scene);
     ground.position.y = -1;
     ground.checkCollisions = true;
+    ground.IsGround = true
+    const ground2 = BABYLON.MeshBuilder.CreateGround('jump1', {width: 2, height:2}, scene);
+    ground2.position.z = -7;
+    ground2.checkCollisions = true;
+    ground2.IsGround = true
+    const ground3 = BABYLON.MeshBuilder.CreateGround('jump2', {width: 2, height:2}, scene);
+    ground3.position.z = -12;
+    ground3.checkCollisions = true;
+    ground3.IsGround = true
     scene.gravity = new BABYLON.Vector3(0, -1, 0)
     return scene;
 }
@@ -62,27 +73,13 @@ const scene = createScene();
 
 engine.runRenderLoop(function() {
     if (timer) timercount++;
+    if (timer2) timercount2++;
     mouselocked = engine.isPointerLock;
-    // box.moveWithCollisions(new BABYLON.Vector3(0, -0.3, 0), false)
-    // console.log(box.position.y)
-    // let directionf = box.forward;
-    // let directionr = box.right;
-    // let directionl = directionr.scale(-1);
-    // let directionb = directionf.scale(-1);
-    // if (input.forward) {
-    //     box.position.addInPlace(directionf.scale(0.1));
-    // } else if (input.backward) {
-    //     box.position.addInPlace(directionb.scale(0.1));
-    // }
-    // if (input.left) {
-    //     box.position.addInPlace(directionl.scale(0.1));
-    // } else if (input.right) {
-    //     box.position.addInPlace(directionr.scale(0.1));
-    // }
-    // if (input.jump) {
-    //     box.position.addInPlace(new BABYLON.Vector3(0, 1, 0).scale(1))
-    // }
     box.moveWithCollisions(height.scale(drop))
+    console.log(box.position)
+    if (jump && jumpcount > 0) {
+        box.moveWithCollisions(height.scale(-0.6))
+    }
     box.moveWithCollisions(velocity.scale(speed))
     scene.render();
 });
@@ -113,7 +110,15 @@ scene.onBeforeRenderObservable.add(() => {
         timercount = 0;
     }
     velocity.set(0,0,0)
-    if (input.jump) box.position.y = 1;
+    if (jump && !isGrounded) {
+        jumpcount--;
+    }
+    if (input.jump) {
+        if (isGrounded) {
+            jump = true
+            jumpcount = 8
+        }
+    }
     if (input.forward) {
         const directionf = box.getDirection(new BABYLON.Vector3(0, 0, 1))
         velocity.addInPlace(directionf)
@@ -128,31 +133,38 @@ scene.onBeforeRenderObservable.add(() => {
         const directionr = box.getDirection(new BABYLON.Vector3(1, 0, 0))
         velocity.addInPlace(directionr)
     }
-    speed = timercount/400
+    let multipler = 1
+    if (input.shift) multipler = 1.5;
+    speed = timercount/400 * multipler
     // console.log(timercount)
     // console.log(velocity)
-    speed = BABYLON.Scalar.Clamp(speed, 0, 0.5)
-    console.log(speed)
+    speed = BABYLON.Scalar.Clamp(speed, 0, 0.3 * multipler)
+    // console.log(speed)
 });
 
 scene.onBeforeRenderObservable.add(() => {
     const origin = box.position.clone()
     origin.y -= box.ellipsoid.y
     const direction = new BABYLON.Vector3(0, -1, 0)
-    const ray = new BABYLON.Ray(origin, direction, 1)
-    const hit = scene.pickWithRay(ray, (box) => {
-        return box === ground;
+    const ray = new BABYLON.Ray(origin, direction, 0.1)
+    const hit = scene.pickWithRay(ray, (mesh) => {
+        return mesh.IsGround
     });
-    if (hit.hit) {
+    if (hit.pickedMesh) {
         isGrounded = true
     }else {
         isGrounded = false
     }
-    console.log(isGrounded)
+    // console.log(isGrounded)
     if (!isGrounded) {
-        startTimer2();  
-        drop = gravity * timercount2/100
-    }  
+        startTimer2(); 
+        // console.log(timercount2) 
+        drop = gravity * timercount2/500
+        // console.log(drop)
+    } else {
+        timer2 = false
+        timercount2 = 0
+    }
 });
 
 const startTimer = function() {
@@ -188,26 +200,30 @@ scene.onPointerObservable.add((pointerInfo) => {
 });
 
 
-window.addEventListener('keydown', function(event) {;
-    if (event.key === 'w') {
+window.addEventListener('keydown', function(event) {
+    const key = event.key.toLowerCase()
+    if (key === 'w') {
         input.forward = true;
         if (!timer) startTimer();
         // inputs = true;
     }
-    if (event.key === 's') {
+    if (key === 's') {
         input.backward = true;
         if (!timer) startTimer();
         // inputs = true;
     }
-    if (event.key === 'a') {
+    if (key === 'a') {
         input.left = true;
         if (!timer) startTimer();
         // inputs = true;
     }
-    if (event.key === 'd') {
+    if (key === 'd') {
         input.right = true;
         if (!timer) startTimer();
         // inputs = true;  
+    }
+    if (event.shiftKey) {
+        input.shift = true
     }
     if (event.key === ' ') {
         input.jump = true;
@@ -217,20 +233,24 @@ window.addEventListener('keydown', function(event) {;
 
 
 window.addEventListener('keyup', function(event) {
-    if (event.key === 'w') {
+    const key = event.key.toLowerCase()
+    if (key === 'w') {
         input.forward = false;
     }
-    if (event.key === 's') {
+    if (key === 's') {
         input.backward = false;
     }
-    if (event.key === 'a') {
+    if (key === 'a') {
         input.left = false;
     }
-    if (event.key === 'd') {
+    if (key === 'd') {
         input.right = false;
     }
     if (event.key === ' ') {
         input.jump = false;
+    }
+    if (event.shiftKey) {
+        input.shift = false
     }
 });
 
